eaea4cb72b0808ce2de22cbc3a1b326c442cc53a
yanniser <yannick.serra@darva.com>
2022-05-11 19:23:17 +0200

    sxc

--- dist/index.js
+++ dist/index.js
@@ -8548,7 +8548,7 @@ async function getMileStoneFromEtiquette ( etiquettesTicketJira ) {
 async function run() {
   try {
     const title = github.context.payload.pull_request.title;
-    core.info(`Processing PR___milestone awaiting:${title}  ...`)
+    core.info(`Processing PR___milestone awaiting title:${title}  ...`)
 
     const repo = core.getInput('repo', {required: true})
     const owner = core.getInput('owner', {required: true})
