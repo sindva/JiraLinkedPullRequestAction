fcc29b1035d6be72400c00ca8a8ffda152099c74
yanniser <yannick.serra@darva.com>
2022-05-13 08:12:39 +0200

    fix typo on length

--- dist/index.js
+++ dist/index.js
@@ -8471,61 +8471,59 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const fetch = __nccwpck_require__(467);
+const defaultMilestone = 54;
 
-async function getJiraTicket (ticket , jira_token ) {
-  core.info(`in  getJiraTicket ${ticket} `)
-  const url = "https://support.apps.darva.com/"+'rest/api/2/issue/SINAPPSHAB-'+ticket
+async function getJiraTicket(ticket, jira_token) {
+  core.info(`in  getJiraTicket ${ticket} `);
+  const url =
+    "https://support.apps.darva.com/" + "rest/api/2/issue/SINAPPSHAB-" + ticket;
   const toto = fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Authorization': `Basic ${jira_token}`,
-      'Accept': 'application/json'
-    }
+      Authorization: `Basic ${jira_token}`,
+      Accept: "application/json",
+    },
   })
-  .then(response =>  {
-    const res =response.json()
-    core.info (`response ${res}`)
-    return res
-  })
-  //.then(data => core.info(`fields ${data.fields}` ))
-  .catch(err => core.info(err));
+    .then((response) => {
+      const res = response.json();
+      core.info(`response ${res}`);
+      return res;
+    })
+    .catch((err) => core.info(err));
 
-  return toto
+  return toto;
 }
 
-
-async function getMileStoneFromEtiquette ( etiquettesTicketJira ) {
-  if ( etiquettesTicketJira.includes('FLUOR-BIS') ) {
-      core.info ('on set FLUOR-BIS')
-      return 55
-  } else if (  etiquettesTicketJira.includes('FLUOR') ) {
-      core.info ('on set FLUOR')
-      return 53
-  } else if (  etiquettesTicketJira.includes('PALLADIUM') ) {
-      core.info ('on set PALLADIUM')
-      return 52
-  } else if (  etiquettesTicketJira.includes('ARGON') ) {
-      core.info ('on set ARGON')
-      return 54
-  } else return 54 
+async function getMileStoneFromEtiquette(etiquettesTicketJira) {
+  if (etiquettesTicketJira.includes("FLUOR-BIS")) {
+    core.info("on set FLUOR-BIS");
+    return 55;
+  } else if (etiquettesTicketJira.includes("FLUOR")) {
+    core.info("on set FLUOR");
+    return 53;
+  } else if (etiquettesTicketJira.includes("PALLADIUM")) {
+    core.info("on set PALLADIUM");
+    return 52;
+  } else if (etiquettesTicketJira.includes("ARGON")) {
+    core.info("on set ARGON");
+    return 54;
+  } else return defaultMilestone;
 }
-
-
-
-
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const title = github.context.payload.pull_request.title;
-    core.info(`Processing PR__time passes data:${title}  ...`)
+    core.info(`Processing PR__time passes data:${title}  ...`);
 
-    const jira_token = core.getInput('jira_token', {required: true})
-    const JIRA_TICKETS = JSON.parse( core.getInput('jira_tickets', {required: true}) )
-    
-    core.info(`Processing PR :${title}  ...`)
+    const jira_token = core.getInput("jira_token", { required: true });
+    const JIRA_TICKETS = JSON.parse(
+      core.getInput("jira_tickets", { required: true })
+    );
 
-    if (JIRA_TICKETS.lenght > 0) {
+    core.info(`Processing PR :${title}  ...`);
+    let milestoneNumberToSet = defaultMilestone;
+    if (JIRA_TICKETS.length > 0) {
       const jsonTicket = await getJiraTicket(JIRA_TICKETS[0], jira_token);
       core.info(`after  getJiraTicket`);
       //on récupere la liste des etiquettes du Jira
@@ -8537,16 +8535,12 @@ async function run() {
       );
 
       core.info("Traitement du Milestone:");
-      const milestoneNumberToSet = await getMileStoneFromEtiquette(
+      milestoneNumberToSet = await getMileStoneFromEtiquette(
         etiquettesTicketJira
       );
-      core.info(`we output milestone number:${milestoneNumberToSet}`);
-      core.setOutput("milestone", milestoneNumberToSet);
     }
-
-
-    
-
+    core.info(`we output milestone number:${milestoneNumberToSet}`);
+    core.setOutput("milestone", milestoneNumberToSet);
   } catch (error) {
     core.setFailed(error.message);
   }
